cf9a2f4e92c8d101847008db1fbaf97ddefba0c3
Rokusuke <2438takumi20000918@gmail.com>
2020-10-06 10:47:02 +0900

    Update

--- main.py
+++ main.py
@@ -7,35 +7,38 @@ import random
 import schedule
 
 def enter(browser):
-    for i in range(1,10):
-        browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[2]/div[1]/div/div[2]/div/div[1]/div/div["+str(i*2)+"]/span/div[3]/div/div").click()
-    browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[2]/span/div[3]/div/div").click()
-    browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[4]/span/div[3]/div/div").click()
-    browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[2]/div[3]/div/div[2]/div/span/div/div[1]/label/div/div[1]/div").click()
-    browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[3]/div/div/div").click()
-    sleep(10)
     #36度
-    browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[2]/div[2]/div/div[2]/div/span/div/div[2]/label/div/div[1]/div").click()
+    browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[1]/div[1]").click()
+    sleep(1)
+    browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[2]/div[4]").click()
     #35度
-    #browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[2]/div[2]/div/div[2]/div/span/div/div[1]/label/div/div[1]/div").click()
-    #1:.0,10:.9 default 36.2~36.5
-    param = random.randint(3,6)
-    browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[2]/div[3]/div/div[2]/div/span/div/div["+str(param)+"]/label/div/div[1]/div").click()
-    browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[2]/div[4]/div/div[2]/div/span/div/div[2]/label/div/div[1]/div").click()
-    browser.find_element_by_xpath("//*[@id='mG61Hd']/div/div/div[3]/div/div/div[2]").click()
+    #browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[2]/div[3]).click()
+    _min = 1
+    _max = 3
+    param = random.randint(_max+3,_max+3)
+    sleep(1)
+    browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[1]/div[1]").click()
+    sleep(1)
+    browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div["+str(param)+"]").click()
+    sleep(1)
+    browser.find_element_by_xpath("//*[@id='i16']/div[3]/div").click()
+    browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[3]/div/div/div/span").click()
+    sleep(10)
+    browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span").click()
     sleep(10)
     return
+
 
 def login(user_data,browser):
     sleep(10)
     #メールアドレス入力
     browser.find_element_by_name("identifier").send_keys(user_data[0])
     browser.find_element_by_xpath("//*[@id='identifierNext']/div").click()
-    sleep(20)
+    sleep(10)
     #パスワード入力
     browser.find_element_by_name("password").send_keys(user_data[1])
     browser.find_element_by_xpath("//*[@id='passwordNext']/div").click()
-    sleep(20)
+    sleep(10)
     return
 
 def main():
@@ -65,10 +68,13 @@ if __name__ == "__main__":
         time = data["time"]
     except:
         time = "10:00"
+    '''
     try:
         schedule.every().day.at(time).do(main)
     except:
         print("[-]Process failed")
     while True:
         schedule.run_pending()
+    '''
+    main()
     sleep(1)
